ff68e16430dfdbb304a66e15634a20b54c4b67e5
MB <mbarry323@gmail.com>
2018-06-23 03:48:20 -0400

    created js index and readme fiels

---
+++ README.md
@@ -0,0 +1,1 @@
+//This small exercise goes through the creation of a function that will test what is passed into it to see if it's a palindrome. If it's a palendrome it will return true. Otherwise it will return false.
---
+++ js/palindrome.js
@@ -0,0 +1,16 @@
+//Create a function that will test what we pass to it to see if it's a palindrome. If true, return.. if false return,
+
+function checkForPalindrome(str){
+  var tester = str;
+  tester = tester.split(""); //what about the object? oh split it.
+  tester = tester.reverse(); //what about the object? oh reverse it.
+  tester = tester.join(""); //what about the object? oh join it.
+
+  if (tester === str) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+checkForPalindrome ('bobo');
